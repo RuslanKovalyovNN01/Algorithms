606463eded104aecaecdbafbfea44a22df81384c
Kovalyov Ruslan <i-am-kovalyov-ruslan@yandex.ru>
2026-08-21 19:28:32 +0300

    add test double vector

--- Sorting/Sorting/Sorting/quadratic_sorts.h
+++ Sorting/Sorting/Sorting/quadratic_sorts.h
@@ -13,3 +13,17 @@ void bubble_sort(std::vector<T>& nums) {
 		}
 	}
 }
+
+template<typename T>
+void selection_sort(std::vector<T>& nums) {
+	const int size = nums.size();
+	for (int i = 0; i < size; i++) {
+		int index_min = i;
+		for (int j = i + 1; j < size; j++) {
+			if (nums[j] < nums[index_min]) {
+				index_min = j;
+			}
+		}
+		std::swap(nums[i], nums[index_min]);
+	}
+}
